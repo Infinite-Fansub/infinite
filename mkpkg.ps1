125c7e82abdbbe7b61512cbd83d7f32bd9c897f8
DidaS <didasoficial@gmail.com>
2022-03-06 21:24:39 +0000

    [INITIAL] Added essential scripts

---
+++ mkpkg.ps1
@@ -0,0 +1,71 @@
+#! /usr/bin/pwsh
+
+$root = Get-Location
+
+# we need to be where we hold all of the packages
+Set-Location .\packages
+
+# get package info
+$packageName = Read-Host -Prompt "What do you want to call the new package?"
+Write-Output $packageName
+$description = Read-Host -Prompt "What should the description of the package be?"
+
+# create and enter
+New-Item -Path ".\$packageName" -ItemType Directory
+Set-Location ('.\' + $packageName)
+
+# set up source
+New-Item -Path '.\src' -ItemType Directory
+New-Item -Path '.\src\index.ts' -ItemType File -Value '// Entry point of the project`n'
+New-Item -Path '.\src\typings' -ItemType Directory
+
+# set up tests
+New-Item -Path '.\tests' -ItemType Directory
+New-Item -Path '.\tests\index.ts' -ItemType File -Value '// Main tests here`n'
+
+# ignore files
+New-Item -Path '.\.gitignore' -ItemType File -Value "node_modules/`n.DS_Store`ndist/`n.npmignore`n"
+New-Item -Path '.\.npmignore' -ItemType File -Value "tests/`nsrc/`n.gitignore`n"
+
+# npm files
+New-Item -Path '.\package.json' -ItemType File -Value "{
+    `"name`": `"@infinite/$packageName`",
+    `"version`": `"1.0.0`",
+    `"description`": `"$description`",
+    `"author`": `"Infinite`",
+    `"license`": `"AGPL-3.0`",
+    `"main`": `"dist/index.js`",
+    `"types`": `"dist`",
+    `"scripts`": {
+        `"test`": `"ts-node tests/index.ts`",
+        `"eslint`":`"eslint`",
+        `"eslint:fix`":`"eslint --fix`",
+        `"build`": `"tsc`",
+        `"build:test`": `"tsc --noEmit`",
+        `"run:node`": `"node .`",
+        `"run:ts`": `"ts-node src/index.ts`"
+    },
+    `"repository`": {
+        `"type`": `"git`",
+        `"url`": `"https://github.com/Infinite-Fansub/infinite.git`"
+    },
+    `"homepage`": `"https://github.com/Infinite-Fansub/infinite/tree/main/packages/$packageName`"
+}"
+
+# typescript shenanigans
+New-Item -Path '.\tsconfig.json' -ItemType File -Value '{
+    "extends": "../../tsconfig.json",
+    "compilerOptions": {
+        "rootDir": "src",
+        "outDir": "dist",
+        "baseUrl": "."
+    },
+    "include": [
+        "src/**/*"
+    ]
+}'
+
+# go back home
+Set-Location $root
+
+npm.cmd i
---
+++ mkpkg.sh
@@ -0,0 +1,64 @@
+#! /usr/bin/bash
+
+cd packages
+
+echo "What do you want to call the new package?"
+read packageName
+echo "What should the description of the package be?"
+read description
+
+mkdir "$packageName"
+cd "$packageName"
+
+# set up source
+mkdir src && echo '// Entry point of the project' > src/index.ts
+mkdir src/typings
+
+# set up tests
+mkdir tests && echo '// Main tests here' > tests/index.ts
+
+# ignore files
+echo $'node_modules/\n.DS_Store\ndist/\n.npmignore' > .gitignore
+echo $'tests/\nsrc/\n.gitignore' > .npmignore
+
+# npm files
+echo "{
+    \"name\": \"@infinite/$packageName\",
+    \"version\": \"1.0.0\",
+    \"description\": \"$description\",
+    \"author\": \"Infinite\",
+    \"license\": \"AGPL-3.0\",
+    \"main\": \"dist/index.js\",
+    \"types\": \"dist\",
+    \"scripts\": {
+        \"test\": \"ts-node tests/index.ts\",
+        \"eslint\":\"eslint\",
+        \"eslint:fix\":\"eslint --fix\",
+        \"build\": \"tsc\",
+        \"build:test\": \"tsc --noEmit\",
+        \"run:node\": \"node .\",
+        \"run:ts\": \"ts-node src/index.ts\"
+    },
+    \"repository\": {
+        \"type\": \"git\",
+        \"url\": \"https://github.com/Infinite-Fansub/infinite.git\"
+    },
+    \"homepage\": \"https://github.com/Infinite-Fansub/infinite/tree/main/packages/$packageName\"
+}" > package.json
+
+# typescript shenanigans
+echo '{
+    "extends": "../../tsconfig.json",
+    "compilerOptions": {
+        "rootDir": "src",
+        "outDir": "dist",
+        "baseUrl": "."
+    },
+    "include": [
+        "src/**/*"
+    ]
+}' > tsconfig.json
+
+cd ../..
+
+npm i
