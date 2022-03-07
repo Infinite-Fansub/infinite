4dbc7a23dfac01f9ce906b3c7af392560def4680
DidaS <80978939+Didas-git@users.noreply.github.com>
2022-03-07 00:09:13 +0000

    [BETA] Added dependencies and fixed test

--- packages/fetch/tests/index.ts
+++ packages/fetch/tests/index.ts
@@ -2,6 +2,6 @@ import fetch from "../src";
 
 (async () => {
     const res = await fetch("https://nekos.life/api/v2/img/neko");
-    if (res.json()) console.log("Test Completed: fetch");
+    if (res.rawData.length) console.log("Test Completed: fetch");
     else console.log("Test Failed");
 })();
