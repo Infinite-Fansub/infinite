4ab8d2bc2235834e980f6cdac5e595f52fde781f
DidaS <80978939+Didas-git@users.noreply.github.com>
2022-03-25 21:02:39 +0000

    [REPO] Moved psw and sh scripts to rust

---
+++ rust/src/constants.rs
@@ -0,0 +1,20 @@
+pub const SRC_INDEX_CONTENT: &str = "export class Main {
+    public test(log: string): string {
+        return log;
+    }
+}
+
+new Main().test(\"Hello World!\");";
+
+pub const TESTS_INDEX_CONTENT: &str = "import { Main } from \"../src\";
+import assert from \"node:assert\";
+
+assert.deepStrictEqual(new Main().test(\"Hello World!\"), \"Hello World!\");";
+
+pub const GITIGNORE: &str = "node_modules/
+.DS_Store
+dist/";
+
+pub const NPMIGNORE: &str = "tests/
+src/
+.gitignore";
---
+++ rust/src/functions.rs
@@ -0,0 +1,40 @@
+use std::path::Path;
+
+#[allow(dead_code)]
+pub fn npm_content(pkg_name: String, description: String) -> String {
+    return String::from(format!("{{
+        \"name\": \"@infinite/{}\",
+        \"version\": \"1.0.0\",
+        \"description\": \"{}\",
+        \"author\": \"Infinite\",
+        \"license\": \"AGPL-3.0\",
+        \"main\": \"dist/index.js\",
+        \"types\": \"dist\",
+        \"scripts\": {{
+            \"test\": \"ts-node tests/index.ts\",
+            \"eslint\": \"eslint\",
+            \"eslint:fix\": \"eslint --fix\",
+            \"build\": \"tsc\",
+            \"build:watch\": \"tsc --watch\",
+            \"build:test\": \"tsc --noEmit\",
+            \"node\": \"node .\",
+            \"tsn\": \"ts-node src/index.ts\"
+        }},
+        \"repository\": {{
+            \"type\": \"git\",
+            \"url\": \"https://github.com/Infinite-Fansub/infinite.git\"
+        }},
+        \"homepage\": \"https://github.com/Infinite-Fansub/infinite/tree/main/packages/$packageName\"
+    }}", pkg_name, description))
+}
+
+#[allow(dead_code)]
+pub fn create_project(path: &Path) {
+    std::fs::create_dir_all(&path.join("src/typings")).unwrap(); 
+    std::fs::create_dir(&path.join("tests")).unwrap();
+}
+
+#[allow(dead_code)]
+pub fn create_file(path: &Path, path_string: &str, content: String) {
+    std::fs::write(&path.join(&path_string), content).unwrap();
+}
