2c49c9fd24dbd3621494820e1376050a612f6292
DidaS <didasoficial@gmail.com>
2022-04-18 18:16:04 +0100

    [REVERTED] Added the mkpkg scripts

---
+++ mkpkg.sh
@@ -0,0 +1,65 @@
+#! /usr/bin/bash
+
+cd packages
+
+echo "What do you want to call the new package?"
+read packageName
+echo "What should the description of the package be?"
+read description
+
+mkdir "$packageName"
+cd "$packageName"
+
+# set up source
+mkdir src && echo '// Entry point of the project' > src/index.ts
+mkdir src/typings
+
+# set up tests
+mkdir tests && echo '// Main tests here' > tests/index.ts
+
+# ignore files
+echo $'node_modules/\n.DS_Store\ndist/' > .gitignore
+echo $'tests/\nsrc/\n.gitignore' > .npmignore
+
+# npm files
+echo "{
+    \"name\": \"@infinite/$packageName\",
+    \"version\": \"1.0.0\",
+    \"description\": \"$description\",
+    \"author\": \"Infinite\",
+    \"license\": \"AGPL-3.0\",
+    \"main\": \"dist/index.js\",
+    \"types\": \"dist\",
+    \"scripts\": {
+        \"test\": \"ts-node tests/index.ts\",
+        \"eslint\": \"eslint\",
+        \"eslint:fix\": \"eslint --fix\",
+        \"build\": \"tsc\",
+        \"build:watch\": \"tsc --watch\",
+        \"build:test\": \"tsc --noEmit\",
+        \"node\": \"node .\",
+        \"tsn\": \"ts-node src/index.ts\"
+    },
+    \"repository\": {
+        \"type\": \"git\",
+        \"url\": \"https://github.com/Infinite-Fansub/infinite.git\"
+    },
+    \"homepage\": \"https://github.com/Infinite-Fansub/infinite/tree/main/packages/$packageName\"
+}" > package.json
+
+# typescript shenanigans
+echo '{
+    "extends": "../../tsconfig.json",
+    "compilerOptions": {
+        "rootDir": "src",
+        "outDir": "dist",
+        "baseUrl": "."
+    },
+    "include": [
+        "src/**/*"
+    ]
+}' > tsconfig.json
+
+cd ../..
+
+npm i
