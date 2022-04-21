f0fd51d998b887c24c5bc0ff30efcf15b21b2e9f
DidaS <didasoficial@gmail.com>
2022-04-21 22:07:00 +0100

    [UPDATE] Added custom types and object support for tuple

--- packages/types/src/tuple.ts
+++ packages/types/src/tuple.ts
@@ -1,9 +1,16 @@
+import { float } from "./float";
+import { integer } from "./integer";
+
 type TypesOfTuples = Minimal | Complex;
-type Minimal = string | number | boolean | Date;
-type Complex = Array<string | number | boolean | Date | Array<unknown>>;
+type Minimal = string | number | boolean | Date | float | integer | Record<PropertyKey, unknown>;
+type Complex = Array<string | number | boolean | Date | float | integer | Record<PropertyKey, unknown> | Array<unknown>>;
 
 export type Tuple<T extends TypesOfTuples, K extends Minimal | undefined = undefined> = T extends Minimal
-    ? [T, K]
+    ? K extends undefined
+    ? T extends Record<PropertyKey, unknown>
+    ? [T]
+    : [T]
+    : [T, K]
     : T extends Complex
     ? K extends undefined
     ? T
