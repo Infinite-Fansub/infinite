82afb0885e9a1510d2205a2d85c9ccb0268e8199
DidaS <didasoficial@gmail.com>
2022-05-07 22:31:37 +0100

    [UPDATE] Leaked toke fix :skull:

--- packages/discord-client/tests/index.ts
+++
@@ -1,8 +0,0 @@
-import { InfiniteClient, GatewayIntentBits } from "../src/index";
-
-new InfiniteClient({
-    intents: [GatewayIntentBits.Guilds],
-    useDatabase: true,
-    databaseType: { type: "redis" },
-    token: "OTM1MjcyNjY1NDc4OTg3ODQ3.Ye8Oiw.26PcRU_zAzupNP_L2HACXNEWM6g"
-});
