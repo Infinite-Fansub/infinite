85b4e53e5fd5f73374b8963dde5c60250fbfcaf2
DidaS <didasoficial@gmail.com>
2022-06-20 18:04:00 +0100

    [UPDATE] Changes now apply to the client and not the handlers

--- packages/discord-client/src/utils/normal-handler.ts
+++ packages/discord-client/src/utils/normal-handler.ts
@@ -4,10 +4,6 @@ import { Handler, Event, ICommand, ISlashCommand, IClientEvents } from "src/typi
 import recursiveRead from "./recursive-read";
 
 export class NormalHandler implements Handler {
-    protected commands: Map<string, ICommand> = new Map();
-    protected slashCommands: Map<string, ISlashCommand> = new Map();
-    protected events: Map<string, Event<any>> = new Map();
-
     public constructor(private readonly client: BaseClient) { }
 
     public loadCommands(): void {
@@ -15,7 +11,7 @@ export class NormalHandler implements Handler {
         recursiveRead(this.client.dirs.commands)
             .forEach(async (path) => {
                 const command: ICommand = (await import(path)).default;
-                this.commands.set(command.name, command);
+                this.client.commands.set(command.name, command);
             });
     }
 
@@ -24,7 +20,7 @@ export class NormalHandler implements Handler {
         recursiveRead(this.client.dirs.slashCommands)
             .forEach(async (path) => {
                 const command: ISlashCommand = (await import(path)).default;
-                this.slashCommands.set(command.data.name, command);
+                this.client.slashCommands.set(command.data.name, command);
             });
     }
 
