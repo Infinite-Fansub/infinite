0c3863c6c6f55b93ce4f9893213ed5658da4aa19
DidaS <didasoficial@gmail.com>
2022-07-18 22:12:01 +0100

    [MINOR] Collector Helper improvements
    Shared options added to make life easier

--- packages/discord-client/src/utils/collector-helper.ts
+++ packages/discord-client/src/utils/collector-helper.ts
@@ -3,7 +3,7 @@ import { CollectorOptions, ParseComponentType } from "../typings";
 
 export class CollectorHelper<T extends Exclude<ComponentType, "ActionRow">> {
 
-    public constructor(type: T, private readonly interaction: Interaction) {
+    public constructor(type: T, private readonly interaction: Interaction, private readonly options: CollectorOptions = { time: 18000 }) {
         if (type === 1) throw new Error("Type should be the expected component and not the action row");
         if (type === 2 && !interaction.isButton()) throw new Error(`Expected interaction of type \`Button\` got type \`${interaction.type}\``);
         if (type === 3 && !interaction.isSelectMenu()) throw new Error(`Expected interaction of type \`SelectMenu\` got type \`${interaction.type}\``);
@@ -13,7 +13,7 @@ export class CollectorHelper<T extends Exclude<ComponentType, "ActionRow">> {
     public create(callback: (interaction: ParseComponentType<T>) => Awaitable<void>, options?: CollectorOptions): void {
         if (options && options.time === undefined) options.time = 18000;
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        const collector = this.interaction.channel?.createMessageComponentCollector(options ?? { time: 18000 });
+        const collector = this.interaction.channel?.createMessageComponentCollector(options ?? this.options);
 
         collector?.on("collect", callback);
     }
