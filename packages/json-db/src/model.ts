93daba9415c08208411a31f66d9b59ef64e033cf
DidaS <didasoficial@gmail.com>
2022-08-26 22:36:41 +0100

    [UPDATE] Started search methods

--- packages/json-db/src/model.ts
+++ packages/json-db/src/model.ts
@@ -8,12 +8,15 @@ import { randomUUID } from "node:crypto";
 import { validateData } from "./utils/validate-data";
 
 export class Model<S extends Schema<SchemaDefinition, MethodsDefinition>> {
-    readonly #schema: S;
+    readonly #schema: ExtractSchemaDefinition<S>;
     readonly #readable: boolean;
+    readonly #path: string;
 
-    public constructor(private readonly name: string, data: S, readable: boolean = false) {
-        this.#schema = data;
+    public constructor(name: string, data: S, readable: boolean = false) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        this.#schema = <any>data[schemaData];
         this.#readable = readable;
+        this.#path = resolve(process.env.JSON_DB_PATH ?? process.cwd(), name);
     }
 
     // TODO: #defineMethods()
@@ -21,21 +24,21 @@ export class Model<S extends Schema<SchemaDefinition, MethodsDefinition>> {
     public create(id?: string): Document<ExtractSchemaDefinition<S>> & MapSchema<ExtractSchemaDefinition<S>> {
         // Using `any` because of the MapSchema type
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any, @typescript-eslint/keyword-spacing
-        return <any>new Document(this.#schema[schemaData], id ?? randomUUID());
+        return <any>new Document(this.#schema, id ?? randomUUID());
     }
 
     public async get(id: string): Promise<Document<ExtractSchemaDefinition<S>> & MapSchema<ExtractSchemaDefinition<S>> | null> {
         // we need to get the file that corresponds to `this.name:id` and return a new document
-        const file = await readFile(resolve(process.env.JSON_DB_PATH ?? process.cwd(), `${this.name}-${id}.json`), { encoding: "utf-8" }).catch(() => undefined);
+        const file = await readFile(`${this.#path}/${id}.json`, { encoding: "utf-8" }).catch(() => undefined);
         if (!file) return null;
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any, @typescript-eslint/keyword-spacing
-        return <any>new Document(this.#schema[schemaData], id, file);
+        return <any>new Document(this.#schema, id, file);
     }
 
     public async save(doc: Document<ExtractSchemaDefinition<S>>): Promise<void> {
-        // eslint-disable-next-line @typescript-eslint/keyword-spacing
-        validateData(doc, this.#schema[schemaData]);
+        // eslint-disable-next-line @typescript-eslint/keyword-spacing, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+        validateData(doc, <any>this.#schema);
         // saves the document to its corresponding file, if it doesnt exist we create the file and save
-        await writeFile(resolve(process.env.JSON_DB_PATH ?? process.cwd(), `${this.name}-${doc._id}.json`), doc.toString(!!this.#readable));
+        await writeFile(`${this.#path}/${doc._id}.json`, doc.toString(!!this.#readable));
     }
 }
--- packages/json-db/src/utils/index.ts
+++ packages/json-db/src/utils/index.ts
@@ -1,5 +1,2 @@
-export * from "./ordered-array";
-export * from "./unordered-array";
-export * from "./match";
 export * from "./parsing-errors";
 export * from "./symbols";
